7e4fcb7a88d78705f2336ee2e97f001a4d52c0f7
Robert Carr <86175637+rhc-iv@users.noreply.github.com>
2023-02-06 09:49:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,45 @@
-board = [" " for x in range(9)]
+def display_board(board):
+    print(f" {board[0]} | {board[1]} | {board[2]} ")
+    print("---+---+---")
+    print(f" {board[3]} | {board[4]} | {board[5]} ")
+    print("---+---+---")
+    print(f" {board[6]} | {board[7]} | {board[8]} ")
 
-def print_board():
-    row1 = "| {} | {} | {} |".format(board[0], board[1], board[2])
-    row2 = "| {} | {} | {} |".format(board[3], board[4], board[5])
-    row3 = "| {} | {} | {} |".format(board[6], board[7], board[8])
+def get_move(board, player):
+    while True:
+        move = int(input("Enter your move (1-9): ")) - 1
+        if move >= 0 and move <= 8 and board[move] == " ":
+            board[move] = player
+            return
+        print("Invalid move, try again.")
 
-    print()
-    print(row1)
-    print(row2)
-    print(row3)
-    print()
+def has_winner(board):
+    wins = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ]
+    for win in wins:
+        if all(board[pos] == "X" for pos in win):
+            return "X"
+        if all(board[pos] == "O" for pos in win):
+            return "O"
+    return None
 
-def player_move(icon):
-    if icon == "X":
-        number = 1
-    elif icon == "O":
-        number = 2
-    print("Your turn player {}".format(number))
-    choice = int(input("Enter your move (1-9): ").strip())
-    if board[choice - 1] == " ":
-        board[choice - 1] = icon
-    else:
-        print()
-        print("That space is already taken!")
-
-def is_victory(icon):
-    if (board[0] == icon and board[1] == icon and board[2] == icon) or \
-       (board[3] == icon and board[4] == icon and board[5] == icon) or \
-       (board[6] == icon and board[7] == icon and board[8] == icon) or \
-       (board[0] == icon and board[3] == icon and board[6] == icon) or \
-       (board[1] == icon and board[4] == icon and board[7] == icon) or \
-       (board[2] == icon and board[5] == icon and board[8] == icon) or \
-       (board[0] == icon and board[4] == icon and board[8] == icon) or \
-       (board[2] == icon and board[4] == icon and board[6] == icon):
-        return True
-    else:
-        return False
-
-def is_draw():
-    if " " not in board:
-        return True
-    else:
-        return False
+board = [" " for _ in range(9)]
+display_board(board)
 
 while True:
-    print_board()
-    player_move("X")
-    print_board()
-    if is_victory("X"):
-        print("X wins! Congratulations!")
+    get_move(board, "O")
+    display_board(board)
+    winner = has_winner(board)
+    if winner:
+        print(f"{winner} wins!")
         break
-    elif is_draw():
-        print("It's a draw!")
-        break
-    player_move("O")
-    if is_victory("O"):
-        print_board()
-        print("O wins! Congratulations!")
-        break
-    elif is_draw():
-        print("It's a draw!")
+
+    get_move(board, "X")
+    display_board(board)
+    winner = has_winner(board)
+    if winner:
+        print(f"{winner} wins!")
         break
